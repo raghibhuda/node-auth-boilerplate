57cdab5239dca718d4bde74ceea7746db0918025
Ragib Ahsanul <raghibhuda@gmail.com>
2019-05-04 18:46:14 +0600

    phone verification issues fixed

--- server/controllers/phoneVerification.js
+++ server/controllers/phoneVerification.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import model from '../models';
 import jwt from 'jsonwebtoken';
 import passport from 'passport';
@@ -5,9 +6,8 @@ import getToken from '../config/tokenChecker';
 
 const { User } = model;
 require('../config/passport')(passport);
-// require('dotenv').config();
 
-const authy = require('authy')('OwxJ43qfI6XlEH6KnzGl3fBvHy1EcSag')
+const authy = require('authy')(process.env.AUTHY_API_KEY)
 
 class PhoneVerifier {
 
@@ -16,35 +16,42 @@ class PhoneVerifier {
         const phone = req.body.phone;
         const country_code = req.body.country_code;
         const via = "sms";
-        let authy_response = {};
         if (token) {
-            authy.phones().verification_start(phone, country_code, via, function (error, response) {
-                if (error) {
-                    console.log(error);
-                    authy_response = {
-                        type: 'failed',
-                        error: error
-                    };
-                } else {
-                    console.log(response);
-                    authy_response = {
-                        type: 'success',
-                        response: response
-                    };
-                }
-            });
-            
-            if (authy_response.success === true ) {
-                return res.status(200).send({
-                    success:true,
-                    message:'Verification Code send to your phone '
-                });
-            } else {
-                return res.status(400).send({
-                    success:false,
-                    message:'Something is wrong'
-                });
-            }
+            const getCurrentUser = jwt.decode(token);
+            let userId = getCurrentUser.id;
+            return User
+                .findOne({
+                    where: {
+                        id: userId
+                    }
+                }).then((user) => {
+                    if (user.phone_verified === true) {
+                        return res.status(201).send({
+                            success: true,
+                            message: 'Phone number already verifed'
+                        })
+                    } else {
+                        authy.phones().verification_start(phone, country_code, via, function (error, response) {
+                            if (error) {
+                                console.log(error);
+                                return res.status(401).send({
+                                    success: false,
+                                    message: 'Something went wrong.....',
+                                    error: error
+                                })
+
+                            } else {
+                                console.log(response);
+                                return res.status(200).send({
+                                    success: true,
+                                    message: 'Verification Code send to your phone ',
+                                    response: response
+                                })
+                            }
+                        });
+                    }
+                })
+                .catch((error) => res.status(401).send(error));
 
         } else {
             return res.status(403).send({
@@ -53,36 +60,63 @@ class PhoneVerifier {
             });
         }
     }
+
+
 
     static phoneVerification(req, res) {
         let token = getToken(req.headers);
         const phone = req.body.phone;
         const country_code = req.body.country_code;
         const verification_code = req.body.verification_code;
-        let authy_response = {};
+        let phoneNumber = country_code + phone;
+
         if (token) {
+            const getCurrentUser = jwt.decode(token);
+            let userId = getCurrentUser.id;
+            let authy_response = {};
+
+            let userVerification = () => {
+                return User
+                    .findOne({
+                        where: {
+                            id: userId
+                        }
+                    }).then((user) => {
+                        return user
+                            .update({
+                                phone_verified: true,
+                                phone_number: phoneNumber
+                            }).then(response => {
+                                return res.status(200).send({
+                                    success: true,
+                                    message: 'Phone verified successfully',
+                                    response
+                                });
+                            })
+                            .catch((error) => res.status(401).send(error));
+                    })
+                    .catch((error) => res.status(401).send(error));
+            }
+
             authy.phones().verification_check(phone, country_code, verification_code, function (error, response) {
-                if (error) {  
-                    console.log(error);
+                if (error) {
                     authy_response = {
                         type: 'failed',
                         error: error
                     };
-                    console.log(authy_response);
+                    return authy_response;
                 } else {
                     console.log(response);
                     authy_response = {
                         type: 'success',
                         response: response
                     };
-                    console.log(authy_response);
+                    userVerification();
+                    return authy_response;
                 }
             });
-            if (authy_response.type === 'success') {
-                return res.status(200).send(authy_response);
-            } else {
-                return res.status(400).send(authy_response);
-            }
+
+
         } else {
             return res.status(403).send({
                 success: false,
@@ -90,6 +124,9 @@ class PhoneVerifier {
             });
         }
     }
+
+
 }
 
 export default PhoneVerifier;
+
